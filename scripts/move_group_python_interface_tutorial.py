bec8e8c1b30be5a4e6c85cb87f037beaee0405c5
Austin Lawrence <ablarry91@gmail.com>
2017-05-31 10:51:44 -0400

    Tested to work.  Disabled right gripper.

--- scripts/move_group_python_interface_tutorial.py
+++ scripts/move_group_python_interface_tutorial.py
@@ -115,10 +115,6 @@ def move_group_python_interface_tutorial():
   ## end-effector
   print "============ Generating plan 1"
   pose_target = geometry_msgs.msg.Pose()
-  # pose_target.orientation.w = 1.0
-  # pose_target.position.x = 0.257
-  # pose_target.position.y = 0.002
-  # pose_target.position.z = 0.445
   pose_target.orientation.w = 1.0
   pose_target.position.x = 0.257
   pose_target.position.y = 0.002
@@ -138,15 +134,15 @@ def move_group_python_interface_tutorial():
   ## You can ask RVIZ to visualize a plan (aka trajectory) for you.  But the
   ## group.plan() method does this automatically so this is not that useful
   ## here (it just displays the same trajectory again).
-  print "============ Visualizing plan1"
-  display_trajectory = moveit_msgs.msg.DisplayTrajectory()
+  # print "============ Visualizing plan1"
+  # display_trajectory = moveit_msgs.msg.DisplayTrajectory()
 
-  display_trajectory.trajectory_start = robot.get_current_state()
-  display_trajectory.trajectory.append(plan1)
-  display_trajectory_publisher.publish(display_trajectory);
+  # display_trajectory.trajectory_start = robot.get_current_state()
+  # display_trajectory.trajectory.append(plan1)
+  # display_trajectory_publisher.publish(display_trajectory);
 
-  print "============ Waiting while plan1 is visualized (again)..."
-  rospy.sleep(5)
+  # print "============ Waiting while plan1 is visualized (again)..."
+  # rospy.sleep(5)
 
 
   ## Moving to a pose goal
@@ -163,98 +159,98 @@ def move_group_python_interface_tutorial():
   # Uncomment below line when working with a real robot
   group.go(wait=True)
 
-  ## Planning to a joint-space goal 
-  ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  ##
-  ## Let's set a joint space goal and move towards it. 
-  ## First, we will clear the pose target we had just set.
-
-  group.clear_pose_targets()
-
-  ## Then, we will get the current set of joint values for the group
-  group_variable_values = group.get_current_joint_values()
-  print "============ Joint values: ", group_variable_values
-
-  ## Now, let's modify one of the joints, plan to the new joint
-  ## space goal and visualize the plan
-  # group_variable_values[0] = 1.0
-  group_variable_values[0] = 1.93
-  group_variable_values[1] = 1.96
-  group_variable_values[2] = -1.60
-  group_variable_values[3] = 3.08
-  group.set_joint_value_target(group_variable_values)
-
-  plan2 = group.plan()
-
-  print "============ Waiting while RVIZ displays plan2..."
-  rospy.sleep(5)
-
-  ##
-  ## Let's set a joint space goal and move towards it. 
-  ## First, we will clear the pose target we had just set.
+  # ## Planning to a joint-space goal 
+  # ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  # ##
+  # ## Let's set a joint space goal and move towards it. 
+  # ## First, we will clear the pose target we had just set.
 
   # group.clear_pose_targets()
 
-  ## Then, we will get the current set of joint values for the group
-  group_variable_values = group.get_current_joint_values()
-  print "============ Joint values: ", group_variable_values
+  # ## Then, we will get the current set of joint values for the group
+  # group_variable_values = group.get_current_joint_values()
+  # print "============ Joint values: ", group_variable_values
 
-  ## Now, let's modify one of the joints, plan to the new joint
-  ## space goal and visualize the plan
-  # group_variable_values[0] = 1.0
-  group_variable_values[0] = 1.93
-  group_variable_values[1] = 1.70
-  group_variable_values[2] = -1.45
-  group_variable_values[3] = 3.29
-  group.set_joint_value_target(group_variable_values)
+  # ## Now, let's modify one of the joints, plan to the new joint
+  # ## space goal and visualize the plan
+  # # group_variable_values[0] = 1.0
+  # group_variable_values[0] = 1.93
+  # group_variable_values[1] = 1.96
+  # group_variable_values[2] = -1.60
+  # group_variable_values[3] = 3.08
+  # group.set_joint_value_target(group_variable_values)
 
-  plan2 = group.plan()
+  # plan2 = group.plan()
 
-  print "============ Waiting while RVIZ displays plan2 POINT 1..."
-  rospy.sleep(5)
+  # print "============ Waiting while RVIZ displays plan2..."
+  # rospy.sleep(5)
 
-  ## Cartesian Paths
-  ## ^^^^^^^^^^^^^^^
-  ## You can plan a cartesian path directly by specifying a list of waypoints 
-  ## for the end-effector to go through.
-  waypoints = []
+  # ##
+  # ## Let's set a joint space goal and move towards it. 
+  # ## First, we will clear the pose target we had just set.
 
-  # start with the current pose
-  waypoints.append(group.get_current_pose().pose)
+  # # group.clear_pose_targets()
 
-  # first orient gripper and move forward (+x)
-  wpose = geometry_msgs.msg.Pose()
-  wpose.orientation.w = 1.0
-  wpose.position.x = waypoints[0].position.x + 0.1
-  wpose.position.y = waypoints[0].position.y
-  wpose.position.z = waypoints[0].position.z
-  waypoints.append(copy.deepcopy(wpose))
+  # ## Then, we will get the current set of joint values for the group
+  # group_variable_values = group.get_current_joint_values()
+  # print "============ Joint values: ", group_variable_values
 
-  # second move down
-  wpose.position.z -= 0.10
-  waypoints.append(copy.deepcopy(wpose))
+  # ## Now, let's modify one of the joints, plan to the new joint
+  # ## space goal and visualize the plan
+  # # group_variable_values[0] = 1.0
+  # group_variable_values[0] = 1.93
+  # group_variable_values[1] = 1.70
+  # group_variable_values[2] = -1.45
+  # group_variable_values[3] = 3.29
+  # group.set_joint_value_target(group_variable_values)
 
-  # third move to the side
-  wpose.position.y += 0.05
-  waypoints.append(copy.deepcopy(wpose))
+  # plan2 = group.plan()
 
-  ## We want the cartesian path to be interpolated at a resolution of 1 cm
-  ## which is why we will specify 0.01 as the eef_step in cartesian
-  ## translation.  We will specify the jump threshold as 0.0, effectively
-  ## disabling it.
-  (plan3, fraction) = group.compute_cartesian_path(
-                               waypoints,   # waypoints to follow
-                               0.01,        # eef_step
-                               0.0)         # jump_threshold
+  # print "============ Waiting while RVIZ displays plan2 POINT 1..."
+  # rospy.sleep(5)
+
+  # ## Cartesian Paths
+  # ## ^^^^^^^^^^^^^^^
+  # ## You can plan a cartesian path directly by specifying a list of waypoints 
+  # ## for the end-effector to go through.
+  # waypoints = []
+
+  # # start with the current pose
+  # waypoints.append(group.get_current_pose().pose)
+
+  # # first orient gripper and move forward (+x)
+  # wpose = geometry_msgs.msg.Pose()
+  # wpose.orientation.w = 1.0
+  # wpose.position.x = waypoints[0].position.x + 0.1
+  # wpose.position.y = waypoints[0].position.y
+  # wpose.position.z = waypoints[0].position.z
+  # waypoints.append(copy.deepcopy(wpose))
+
+  # # second move down
+  # wpose.position.z -= 0.10
+  # waypoints.append(copy.deepcopy(wpose))
+
+  # # third move to the side
+  # wpose.position.y += 0.05
+  # waypoints.append(copy.deepcopy(wpose))
+
+  # ## We want the cartesian path to be interpolated at a resolution of 1 cm
+  # ## which is why we will specify 0.01 as the eef_step in cartesian
+  # ## translation.  We will specify the jump threshold as 0.0, effectively
+  # ## disabling it.
+  # (plan3, fraction) = group.compute_cartesian_path(
+  #                              waypoints,   # waypoints to follow
+  #                              0.01,        # eef_step
+  #                              0.0)         # jump_threshold
                                
-  print "============ Waiting while RVIZ displays plan3..."
-  rospy.sleep(5)
+  # print "============ Waiting while RVIZ displays plan3..."
+  # rospy.sleep(5)
 
  
-  ## Adding/Removing Objects and Attaching/Detaching Objects
-  ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  ## First, we will define the collision object message
-  collision_object = moveit_msgs.msg.CollisionObject()
+  # ## Adding/Removing Objects and Attaching/Detaching Objects
+  # ## ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  # ## First, we will define the collision object message
+  # collision_object = moveit_msgs.msg.CollisionObject()
 
 
 
